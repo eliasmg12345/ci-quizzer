7bd40fe935024522ba8aad410dc4be88734f0542
eliasmg12345 <randyofgod666@gmail.com>
2022-03-08 15:41:32 -0400

    3

--- db/ci-quizzer.sql
+++ db/ci-quizzer.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Host:                         localhost
+-- Host:                         127.0.0.1
 -- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
 -- SO del servidor:              Win64
 -- HeidiSQL Versión:             11.2.0.6213
@@ -12,11 +12,37 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para ci-quizzer
+DROP DATABASE IF EXISTS `ci-quizzer`;
+CREATE DATABASE IF NOT EXISTS `ci-quizzer` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `ci-quizzer`;
+
+-- Volcando estructura para tabla ci-quizzer.choices
+DROP TABLE IF EXISTS `choices`;
+CREATE TABLE IF NOT EXISTS `choices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `choice_text` text COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
 -- Volcando datos para la tabla ci-quizzer.choices: ~0 rows (aproximadamente)
+DELETE FROM `choices`;
 /*!40000 ALTER TABLE `choices` DISABLE KEYS */;
 /*!40000 ALTER TABLE `choices` ENABLE KEYS */;
 
+-- Volcando estructura para tabla ci-quizzer.questions
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE IF NOT EXISTS `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_text` text COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
 -- Volcando datos para la tabla ci-quizzer.questions: ~0 rows (aproximadamente)
+DELETE FROM `questions`;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 
